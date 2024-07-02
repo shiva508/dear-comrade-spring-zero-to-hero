e5c8d9120bbef306bb9baa4087d0e3c17992fa2f
shiva508 <dasarishiva1@gmail.com>
2024-07-02 07:23:15 +0530

    first commit

---
+++ src/test/java/com/comrade/DearComradeSpringZeroToHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DearComradeSpringZeroToHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
