d164654fe2a6fb051dcd35add2f15711da9b1442
shiva508 <dasarishiva1@gmail.com>
2025-05-19 22:20:29 +0530

    Spring client POC

---
+++ job_portal_service/src/main/java/com/comrade/JobPortalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.comrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobPortalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobPortalServiceApplication.class, args);
+    }
+
+}
